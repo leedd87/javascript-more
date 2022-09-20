be2a41f8f61ae23571084099ef8a47330271bde5
leedd87 <leedav87@gmail.com>
2022-09-20 16:43:17 -0300

    tweets armado

--- 19-PROYECTO-LocalStorage/js/app.js
+++ 19-PROYECTO-LocalStorage/js/app.js
@@ -1,6 +1,6 @@
 //Variables
 const formulario = document.querySelector("#formulario");
-const listTweets = document.querySelector("#lista-tweets");
+const listaTweets = document.querySelector("#lista-tweets");
 let tweets = [];
 
 //Event Listeners
@@ -18,8 +18,63 @@ function agregarTweet(e) {
 
 	//validacion
 	if (tweet === "") {
-		console.log("No puede ir vacio");
+		mostrarError("No puede ir vacio");
 		return;
 	}
-	console.log("agregando tweet");
+
+	const tweetObj = {
+		id: Date.now(), //para que cada tweet tenga un id unico
+		//texto: tweet,
+		tweet,
+	};
+
+	//Agregar tweets
+	tweets = [...tweets, tweetObj];
+
+	//Agregar los tweets, creando el HTML
+	crearHtml();
+
+	//Reiniciar el formulario
+	formulario.reset();
+}
+
+//Mostrar mensaje de error
+function mostrarError(error) {
+	const mensajeError = document.createElement("p");
+	mensajeError.textContent = error;
+	mensajeError.classList.add("error");
+
+	//Insertar en DOM
+	const contenido = document.querySelector("#contenido");
+	contenido.appendChild(mensajeError);
+
+	//Elimina la alerta despues de 3sg
+	setTimeout(() => {
+		mensajeError.remove();
+	}, 1500);
+}
+
+//Muestra listado de los tweets
+function crearHtml() {
+	limpiarHTML();
+
+	if (tweets.length > 0) {
+		tweets.forEach((tweet) => {
+			//crear el HTML
+			const li = document.createElement("li");
+
+			//aniadir el texto
+			li.innerText = tweet.tweet;
+
+			//insertar en el html
+			listaTweets.appendChild(li);
+		});
+	}
+}
+
+//limpiar el HTML
+function limpiarHTML() {
+	while (listaTweets.firstChild) {
+		listaTweets.removeChild(listaTweets.firstChild);
+	}
 }
