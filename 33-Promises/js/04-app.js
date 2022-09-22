a4da608c40f52f8466adf3e86c2a73f546ef3748
leedd87 <leedav87@gmail.com>
2022-09-21 21:55:15 -0300

    promises js

---
+++ 33-Promises/js/04-app.js
@@ -0,0 +1,26 @@
+const paises = [];
+
+const nuevoPais = (pais) =>
+	new Promise((resolve) => {
+		setTimeout(() => {
+			paises.push(pais);
+			resolve(`Agregado ${pais}`);
+		}, 2000);
+	});
+
+nuevoPais("Alemania")
+	.then((resultado) => {
+		console.log(resultado);
+		console.log(paises);
+
+		return nuevoPais("Francia");
+	})
+	.then((resultado) => {
+		console.log(resultado);
+		console.log(paises);
+		return nuevoPais("Inglaterra");
+	})
+	.then((resultado) => {
+		console.log(resultado);
+		console.log(paises);
+	});
